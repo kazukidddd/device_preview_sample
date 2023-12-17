848248a36507764b99244529bb50aa238731413a
kazuki_kimura <kazukidddd836@gmail.com>
2023-12-17 09:39:51 +0900

    Add device_preview package for previewing app on different devices

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => const MyApp(), // Wrap your app
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
